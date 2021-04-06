c963e857b55acc0da5d43392afb1b0141abdb57d
hiperionlarbre <81634932+hiperionlarbre@users.noreply.github.com>
2021-04-06 17:45:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -76,9 +76,20 @@ class Game():
 		img = Image.open("nsi_computer.PNG")
 		img = img.resize((self.longueur,self.largeur))
 		img = ImageTk.PhotoImage(img)
-		canvas = tk.Canvas(self.fenetre,width=self.longueur, height=self.largeur)
-		canvas.create_image(0, 0, anchor=tk.NW, image=img)
-		canvas.pack()
+		self.canvas = tk.Canvas(self.fenetre,width=self.longueur, height=self.largeur)
+		self.canvas.create_image(0, 0, anchor=tk.NW, image=img)
+		self.canvas.pack()
+		
+		self.frame_epreuve = tk.LabelFrame(self.fenetre, text="EPREUVE", width=57/100*self.longueur, height=56/100*self.largeur, bg="red")
+		self.frame_epreuve.pack()
+		self.window_epreuve = self.canvas.create_window(81,70,window=self.frame_epreuve, anchor=tk.NW)
+		"""
+		tk.Label(self.frame_epreuve, text = "grosse bite de cheval").pack()
+		self.frame_joueurs = tk.LabelFrame(self.canvas, text="JOUEURS")
+		self.frame_joueurs.pack()
+		self.frame_score = tk.LabelFrame(self.canvas, text="SCORE")
+		self.frame_score.pack()
+		"""
 		
 		tk.mainloop()
 
