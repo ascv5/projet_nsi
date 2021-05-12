fce23cecd8572223b8685d30c228e5a3f05e3f71
ascv5 <46599708+ascv5@users.noreply.github.com>
2021-05-12 13:37:47 +0200

    DZEDEZDZE

--- tools.py
+++ tools.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import random
 
 
@@ -14,12 +15,27 @@ def choix_phrase():
 		liste.append(a[:-1])
 	return random.choice(liste)
 
-def ep2_choix_question():
+def ep2_choix_question(theme):
 	liste = []
 	f = open("data/ep2/question.txt", "r")
-	for a in f :
-		liste.append(a[:-1])
+	f = f.read().split("|")
+	f = f[int(theme[2])]
+	f = "".join(f).split("\n")
+	for a in f[2:]:
+		liste.append(a)
 	liste = random.choice(liste)
 	liste = ''.join(liste).split(';')
 	return "§".join(liste)
+
+
+def ep2_choix_theme():
+	f = open("data/ep2/question.txt", "r")
+	f = f.read().split("|")
+	f = random.choice(f)
+	f = "".join(f).split("\n")
+	return f[1].split(";")
+
+
+print(ep2_choix_question(ep2_choix_theme()))
+#print(ep2_choix_question())
 
